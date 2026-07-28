81d1a6592ba005df1689ad3525a9f0753e01c4c9
sri12swa12 <srivastava.swati12@gmail.com>
2026-07-28 14:45:33 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+  public boolean isPalindrome(String s) {
+    int l = 0;
+    int r = s.length() - 1;
+
+    while (l < r) {
+      while (l < r && !isAlphaNum(s.charAt(l))) {
+        l++;
+      }
+      while (l < r && !isAlphaNum(s.charAt(r))) {
+        r--;
+      }
+      if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+        return false;
+      }
+      l++;
+      r--;
+    }
+    return true;
+  }
+
+  public boolean isAlphaNum(char c) {
+    return ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+  }
+}
