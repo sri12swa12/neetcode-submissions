d81ef44447358ce3cf56be526d04d0ff6d5f0203
sri12swa12 <srivastava.swati12@gmail.com>
2026-07-31 15:55:33 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,17 @@
+class Solution {
+  public int maxProfit(int[] prices) {
+    int l = 0;
+    int r = 1;
+    int maxProfit =0;
+
+    while (r < prices.length) {
+      if (prices[l] < prices[r]) {
+        maxProfit = Math.max(maxProfit, prices[r] - prices[l]);
+      } else {
+        l = r;
+      }
+      r++;
+    }
+    return maxProfit;
+  }
+}
